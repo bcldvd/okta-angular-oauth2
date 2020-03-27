cb5b335565b5f51e5c8ad8af57fe93628dcb6546
David <david.bocle@finastra.com>
2020-03-27 08:23:01 +0100

    build(environment): fix prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  oktaConfig: {
+    issuer: 'https://dev-628596.okta.com/oauth2/default',
+    redirectUri: window.location.origin + '/implicit/callback',
+    clientId: '0oa4ycbmq5d6IGjRh4x6',
+    scopes: ['openid', 'profile'],
+    pkce: true,
+  },
 };
